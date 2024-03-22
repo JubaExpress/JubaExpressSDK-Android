7dcfd1c291a5b7c71e7ae0629bae34f6419a91b4
Yasir Tanveer <yasir.tanveer@jubaexpress.com>
2024-03-22 16:40:02 +0400

    Initial project commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.jubaexpresssdksample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.jubaexpresssdksample"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("com.google.android.material:material:1.11.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    /*import JubaExpress SDK File*/
+    implementation("com.github.JubaExpress:JubaExpressSDK:1.0")
+
+    /*Required Libraries*/
+    implementation("com.squareup.retrofit2:retrofit:2.10.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
+    //For Pull To Refresh
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    //For Image Loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("androidx.compose.material3:material3:1.2.1")
+
+    /*____/////////////___*/
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/example/jubaexpresssdksample/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.jubaexpresssdksample
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.example.jubaexpresssdksample.ui.theme.JubaExpressSDKSampleTheme
+import com.jubaexpress.sdk.base.JESDKConfig.initSDK
+import com.jubaexpress.sdk.data.model.requests.CustomerInfo
+import com.jubaexpress.sdk.data.model.requests.CustomerName
+import com.jubaexpress.sdk.data.model.requests.DocumentInfo
+import com.jubaexpress.sdk.data.model.requests.JESDKConfiguration
+import com.jubaexpress.sdk.data.model.response.JESDKFinalTransactionModel
+import com.jubaexpress.sdk.utils.FinalTransactionResponse
+import com.jubaexpress.sdk.utils.SDKEnvironment
+import com.jubaexpress.sdk.utils.getParcelableClass
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JubaExpressSDKSampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    ElevatedButton(
+                        modifier = Modifier.wrapContentSize(),
+                        onClick = { initializeSDKForPayment() }) {
+                        Text(text = "Click to Start Payment")
+                    }
+                }
+            }
+        }
+    }
+
+    private val paymentActivityResults =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.extras?.let {
+                    getParcelableClass(
+                        it, FinalTransactionResponse, JESDKFinalTransactionModel::class.java
+                    )?.let {
+                        Log.d("ReferenceId", it.referenceId)
+                        Log.d("SecretKey", it.secretKey)
+//                        if (it.secretKey.isNotBlank()) {
+//                            viewBinding.startSDK.text = it.referenceId + it.secretKey
+//                            startActivity(Intent(
+//                                this, JESDKFinalReceiptActivity::class.java
+//                            ).apply {
+//                                putExtra(ReferenceId, it.referenceId)
+//                                putExtra(ScreenTitle, "Remittance Details")
+//                            })
+//                        } else {
+//                            showToast("Payment required")
+//                        }
+                    }
+                }
+            }
+        }
+
+    private fun initializeSDKForPayment() {
+        initSDK(
+            activity = this,
+            sdkEnvironment = SDKEnvironment.Development,
+            jESDKConfiguration = JESDKConfiguration(
+                subscriptionKey = getString(R.string.license_key),
+                partnerKey = getString(R.string.authorization_key),
+                customerInfo = CustomerInfo(
+                    cIF = "SlVCQVNXRURFTjp0ZXN0aW5nMkBnbWFpbC5jb20=",
+                    name = CustomerName(
+                        firstName = "Yasir", middleName = "", lastName = "Tanveer"
+                    ),
+                    mobile = "923055142665",
+                    email = "testing@gmail.com",
+                    nationality = "Pakistan",
+                    dateOfBirth = "20/06/1996",
+                    placeOfBirth = "Pakistan",
+                    gender = 0,
+                    DocumentInfo(
+                        documentType = 0,
+                        documentNumber = "34502-9519963-9",
+                        documentIssueDate = "18/01/2023",
+                        documentExpiryDate = "18/01/2033",
+                        documentIssuingCountry = ""
+                    )
+                )
+            ),
+            paymentActivity = paymentActivityResults
+        )
+    }
+
+}
+
+
+
